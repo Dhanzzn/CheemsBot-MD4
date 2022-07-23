374cf6d54dca2c1304447ef0949080b0997c9a85
Dhanzzn <95112071+Dhanzzn@users.noreply.github.com>
2022-07-23 10:34:52 +0800

    Add files via upload

--- settings.js
+++ settings.js
@@ -56,7 +56,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 //owmner v card
 global.vcardowner = ['6289514865606'] //ur owner number
 global.ownername = "ALDI STORE" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.ytname = "YT: ALDI - MD" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
