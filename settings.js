ab241240b71b0783159850cced63322f4d1cf77f
Dhanzzn <95112071+Dhanzzn@users.noreply.github.com>
2022-08-19 13:01:40 +0800

    Add files via upload

--- settings.js
+++ settings.js
@@ -54,15 +54,15 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['6282132330379'] //ur owner number
-global.ownername = "AR4YOU" //ur owner name
+global.vcardowner = ['6289514865606'] //ur owner number
+global.ownername = "ALDI STORE OFFICIAL" //ur owner name
 global.ytname = "YT: ALDI - MD" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['6282132330379']
-global.botname = 'KDD BOT' //ur bot name
+global.owner = ['6289514865606']
+global.botname = 'ALDI MD V4' //ur bot name
 global.linkz = "https://chat.whatsapp.com/CHR31dhtRQFGNsYWpnPKS9" //your theme url which will be displayed on whatsapp
 global.websitex = "My Youtube : I'm Dhanzz" //ur website to be displayed
 global.botscript = 'wa.me/6289514865606' //script link
